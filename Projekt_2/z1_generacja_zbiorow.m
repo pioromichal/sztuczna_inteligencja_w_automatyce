c52a741035b025cb391d6c61a10c8f2b74a303da
rslepowr <01178255@pw.edu.pl>
2024-12-09 21:28:48 +0100

    Skończony punkt 1 - Symulacja procesu

--- Projekt_2/z1_generacja_zbiorow.m
+++ Projekt_2/z1_generacja_zbiorow.m
@@ -1,42 +1,93 @@
+% clear; close all;
+% kk = 4000;
+% dlugosc_skoku = 100;
+% ilosc_skokow = kk/dlugosc_skoku - 1;
+% 
+% %% dane uczące
+% losowe_wartosci = -1 + 2 * rand(1, 40);
+% u_ucz = zeros(kk,1);
+% for i = 1:ilosc_skokow
+%     zakres = (1:dlugosc_skoku) + (dlugosc_skoku*i);
+%     u_ucz(zakres) = losowe_wartosci(i);
+% end
+% 
+% x1km1=0;x2km1=0;
+% y_ucz = zeros(kk,1);
+% for k = 6:kk
+%     [x1km1, x2km1, y_ucz(k)] = symulacja_procesu(x1km1,x2km1, ...
+%         u_ucz(k-5));
+% end
+% 
+% figure; hold on;
+% plot(y_ucz,'.');
+% stairs(u_ucz);
+% 
+% 
+% 
+% %% dane weryfikujące
+% losowe_wartosci = -1 + 2 * rand(1, ilosc_skokow);
+% u_wer = zeros(kk,1);
+% for i = 1:ilosc_skokow
+%     zakres = (1:dlugosc_skoku)+(dlugosc_skoku*i);
+%     u_wer(zakres) = losowe_wartosci(i);
+% end
+% 
+% x1km1=0;x2km1=0;
+% y_wer = zeros(kk,1);
+% for k = 6:kk
+%     [x1km1, x2km1, y_wer(k)] = symulacja_procesu(x1km1,x2km1, ...
+%         u_wer(k-5));
+% end
+% 
+% figure; hold on;
+% plot(y_wer,'.');
+% stairs(u_wer);
+
+
+%% wykresy zbioru uczącego
 clear; close all;
-kk = 4000;
-dlugosc_skoku = 100;
-ilosc_skokow = kk/dlugosc_skoku - 1;
+addpath("dane\");
 
-%% dane uczące
-losowe_wartosci = -1 + 2 * rand(1, 40);
-u_ucz = zeros(kk,1);
-for i = 1:ilosc_skokow
-    zakres = (1:dlugosc_skoku) + (dlugosc_skoku*i);
-    u_ucz(zakres) = losowe_wartosci(i);
-end
-
-x1km1=0;x2km1=0;
-y_ucz = zeros(kk,1);
-for k = 1:kk
-    [x1km1, x2km1, y_ucz(k)] = symulacja_procesu(x1km1,x2km1,u_ucz(k));
-end
+load("dane_ucz.mat");
 
 figure; hold on;
+
+subplot(2,1,1);
+stairs(u_ucz);
+ylabel('u_{ucz}');
+grid on; grid minor;
+
+subplot(2,1,2);
 plot(y_ucz,'.');
-plot(u_ucz);
+ylabel('y_{ucz}');
+xlabel('k');
+ylim([-3.5 1])
+grid on; grid minor;
 
+file_name = sprintf('Wykresy/z1_dane_ucz.pdf');
+exportgraphics(gcf, file_name, 'ContentType', 'vector');
+% close;
 
+%% wykresy zbioru weryfikujacego
+% clear; close all;
+addpath("dane\");
 
-%% dane weryfikujące
-losowe_wartosci = -1 + 2 * rand(1, ilosc_skokow);
-u_wer = zeros(kk,1);
-for i = 1:ilosc_skokow
-    zakres = (1:dlugosc_skoku)+(dlugosc_skoku*i);
-    u_wer(zakres) = losowe_wartosci(i);
-end
-
-x1km1=0;x2km1=0;
-y_wer = zeros(kk,1);
-for k = 1:kk
-    [x1km1, x2km1, y_wer(k)] = symulacja_procesu(x1km1,x2km1,u_wer(k));
-end
+load("dane_wer.mat");
 
 figure; hold on;
+
+subplot(2,1,1);
+stairs(u_wer, LineWidth=0.75);
+ylabel('u_{wer}');
+grid on; grid minor;
+
+subplot(2,1,2);
 plot(y_wer,'.');
-plot(u_wer);
+ylabel('y_{wer}');
+xlabel('k');
+ylim([-3.5 1])
+grid on; grid minor;
+
+file_name = sprintf('Wykresy/z1_dane_wer.pdf');
+exportgraphics(gcf, file_name, 'ContentType', 'vector');
+% close;
--- Projekt_2/z1_statyka.m
+++ Projekt_2/z1_statyka.m
@@ -13,5 +13,30 @@ for i = 1:length(us)
     ys(i) = y(end);
 end
 
-figure;
-plot(us, ys); hold on;
+figure; hold on;
+plot(us, ys, LineWidth=1);
+
+
+% statyka wyznaczona analitycznie, aby mieć pewność że model został
+% wyznaczony poprawnie 
+
+alpha1 = -1.272717; alpha2 = 0.332871; 
+beta1 = 0.028400; beta2 = 0.019723;
+
+for i = 1:length(us)
+    u = us(i);
+    g1 = (exp(5.25*u)-1)/(exp(5.25*u)+1);
+    x1 = g1*(beta1+beta2)/(1+alpha1+alpha2);
+    y(i) = 0.8*(1-exp(-2*x1));
+end
+
+colors = lines(3);
+plot(us, y, '--', LineWidth=1, Color=colors(3,:));
+
+% colors = lines(3);
+xlabel('u');
+ylabel('y');
+grid on; grid minor;
+legend('Metoda ekperymentalna', 'Metoda analityczna', Location='best');
+file_name = sprintf('Wykresy/z1_statyka.pdf');
+exportgraphics(gcf, file_name, 'ContentType', 'vector');
